7fd5e3a0e2c3c9b74cc52f8577a343913add27f9
Erica Wanja <wanjaerica@gmail.com>
2022-07-08 12:40:32 +0300

    RemoveDuplicates II

--- Data structures/RemoveDuplicatesII.js
+++ Data structures/RemoveDuplicatesII.js
@@ -1,23 +1,33 @@
-function deleteDuplicates(head){
-    if(head){
-    let curr = head.next;
-    let prev = head;
+function deleteDuplicates(head) {
+  if (head) {
+    let dummy = { data: 0, next: head };
 
-  while(curr){
-    if(prev.data == curr.data){
-        prev.next = curr.next;
-        curr= curr.next
-    }else{
-        prev= prev.next;
-        curr = curr.next
+    let prev = dummy;
+    let curr = dummy.next;
+    let next = curr.next;
+
+    while (curr) {
+      if (curr && next && curr.data === next.data) {
+        while ( curr.data === next.data) {
+          next = next.next;
+        }
+
+        prev.next = next;
+        curr = next;
+        next = next ? next.next : null;
+      } else {
+        prev = curr;
+        curr = next;
+        next = next ? next.next : null;
+      }
     }
+    return dummy;
   }
-}
-  return head;
 
+  return head;
 }
 let list1 = {
-    data: 1,
-    next: { data: 1, next: { data: 2, next: null } },
-  };
-  console.log(deleteDuplicates(list1))
+  data: 1,
+  next:{ data: 3, next: { data: 3, next:{ data: 4, next: { data: 4, next: { data: 5, next: null } } }}},
+};
+console.log(deleteDuplicates(list1));
